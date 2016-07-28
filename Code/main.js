74e7d18a7038269f768cdb569de06e0be62d0752
13142 <13142@hillcrest-high.school.nz>
2016-07-28 15:50:29 +1200

    PulsingTitle

--- Code/main.js
+++ Code/main.js
@@ -1,18 +1,20 @@
 var brightness = document.getElementById("titleImage");
 var val = 200;
 var rising = false;
-setInterval(function, milliseconds)
-while (true) {
-    if (rising) {
-        val += 1;
-    } else {
-        val -= 1;
-    }
-    if (val > 200) {
-        rising = false;
-    } else if (val < 20) {
-        rising = true;
-    }
+window.setInterval(pulsingTitle, 20);
+
+function pulsingTitle() {
+  if (rising) {
+      val += 0.3333;
+  } else {
+      val -= 0.3333;
+  }
+  if (val > 100) {
+      rising = false;
+  } else if (val < 0) {
+      rising = true;
+  }
+  brightness.style.webkitFilter = "brightness(" + (-45 * Math.cos((Math.PI / 50) * val) + 45 + 60) + "%)";
+  console.log((-45 * Math.cos((Math.pi / 50) * val) + 45));
 
 }
-brightness.style.webkitFilter = "brightness(" + val + "%)";
