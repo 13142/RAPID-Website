4f0a5b72043d755899b561e8115c024aad47628f
13142 <13142@hillcrest-high.school.nz>
2016-08-01 14:22:42 +1200

    Fade up and down

--- Code/main.js
+++ Code/main.js
@@ -1,20 +1,20 @@
 var brightness = document.getElementById("titleImage");
 var val = 200;
 var rising = false;
-window.setInterval(pulsingTitle, 20);
+window.setInterval(pulsingTitle, 10);
 
 function pulsingTitle() {
-  if (rising) {
-      val += 0.3333;
-  } else {
-      val -= 0.3333;
-  }
-  if (val > 100) {
-      rising = false;
-  } else if (val < 0) {
-      rising = true;
-  }
-  brightness.style.webkitFilter = "brightness(" + (-45 * Math.cos((Math.PI / 50) * val) + 45 + 60) + "%)";
-  console.log((-45 * Math.cos((Math.pi / 50) * val) + 45));
+    if (rising) {
+        val += 0.25;
+    } else {
+        val -= 0.25;
+    }
+    if (val > 100) {
+        rising = false;
+    } else if (val < 0) {
+        rising = true;
+    }
+    brightness.style.webkitFilter =
+        "brightness(" + (-20 * Math.cos((Math.PI / 50) * val) + 20 + 85) + "%)";
 
 }
