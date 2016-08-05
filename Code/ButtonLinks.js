e015beef76c5597b939cccd8cf2780e589ae5596
13142 <13142@hillcrest-high.school.nz>
2016-08-05 13:29:50 +1200

    Added updated fonts
    And tried to add button glow animation

--- Code/ButtonLinks.js
+++ Code/ButtonLinks.js
@@ -1,6 +1,6 @@
 function SheppardClick() {
-  window.alert("SheppardClicked");
+    $(".Sheppard").animate({ boxShadow: "0 0 250px 30px rgb(0, 156, 255, 0.7)"});
 }
 function SnellClick() {
-  
+
 }
