5d0c0967dbacfba04e5a4a183f845a3ed1267fc3
13142 <13142@hillcrest-high.school.nz>
2016-08-18 10:00:38 +1200

    Started on the sheppard game

--- Houses/Sheppard/Code/Sheppard.js
+++ Houses/Sheppard/Code/Sheppard.js
@@ -1,4 +1,34 @@
+document.getElementById("sheppardPortrait").playbackRate = 1.6;
+
+$(document).ready(function() {
+  var stage = new PIXI.Container();
+  var renderer = PIXI.autoDetectRenderer(640, 480, {backgroundColor: 0x00c9ff, antialias: true});
+
+  document.getElementById("gameWindow").appendChild(renderer.view);
+  var graphics = new PIXI.Graphics();
+
+  graphics.beginFill(0xfb1717);
+  graphics.drawCircle(60, 80, 15);
+  graphics.endFill();
+
+var x = 0;
+
+  stage.addChild(graphics);
+
+  requestAnimationFrame( animate );
+
+  function animate() {
+      renderer.render(stage);
+
+      graphics.clear();
+x+= 1;
+      graphics.beginFill(0xffffff);
+      graphics.drawCircle(x, 220, 18);
+      graphics.endFill();
+      requestAnimationFrame(animate);
+  }
+});
 
 function FadeOut() {
-$("#loaderWrapper").addClass("loaded");
+    $("#loaderWrapper").addClass("loaded");
 }
