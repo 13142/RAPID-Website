017c5d529d826ac7771c46a7571dcfea3a3ff518
Hand-of-Cthulhu <necrorerxx@gmail.com>
2016-09-04 21:24:39 +1200

    Added video

--- Houses/Sheppard/Code/Sheppard.js
+++ Houses/Sheppard/Code/Sheppard.js
@@ -1,38 +1,37 @@
-//document.getElementById("sheppardPortrait").playbackRate = 1.6;
-$(document).ready(function() {
-    Resize();
+$(document).ready(function()
+{
+
+    $("#loader").hide().delay(400).fadeIn(1500);
+
     var allWords;
 
-    $.ajax({
+    $.ajax(
+    {
         url: "Media/SheppardWords.txt",
         dataType: "text",
-        success: function(data) {
-            allWords = (data).split("\n");
+        success: function(data)
+        {
+            var NewData = data.toUpperCase();
+            allWords = (NewData).split("\n");
+            setTimeout(LoadBoxes, 0);
+            //LoadBoxes();
+            setTimeout(Resize, 300);
 
-            LoadBoxes();
 
-            Resize();
-			setTimeout(Resize, 150);
-			setTimeout(Resize, 300);
-			
-            $(".innerBoxesRow").click(function() {
-                $(this).addClass("magictime puffOut");
-                var thisIs = this;
-                setTimeout(function() {
-                    $(thisIs).addClass('loaded');
-                    $(thisIs).css("pointerEvents", "none");
-                    //  $(thisIs).css("position", "fixed");
-                }, 1000);
-            });
         }
     });
 
-    function LoadBoxes() {
+    function LoadBoxes()
+    {
         var randomRows = GetRandomInt(4, 6);
         var randomCol = GetRandomInt(4, 6);
         var widthy = RandomSplits(100, randomRows, 1, 6);
-        console.log(widthy);
-        for (var y = 0; y < randomRows; y++) {
+        var done = [
+            false,
+            false
+        ];
+        for (var y = 0; y < randomRows; y++)
+        {
             var heightx = RandomSplits(100, randomCol, 1, 6);
             console.log(heightx);
             var randomHeight = GetRandomArbitrary(1, 4);
@@ -44,7 +43,8 @@ $(document).ready(function() {
             var follow2 = "<div id=\'vertBox2-" + y + "' class='innerBoxesCol' style='flex: 0 0 auto; width: " + wInPixels + "px'></div>";
             $(".mainBody").append(follow);
             $(".secondBody").append(follow2);
-            for (var x = 0; x < randomCol; x++) {
+            for (var x = 0; x < randomCol; x++)
+            {
                 var hInPixels = $(".mainBody").height() * (heightx[x] / 100);
                 //var randomWidth = GetRandomArbitrary(1, 4);
                 var randomIndex = GetRandomInt(0, allWords.length);
@@ -54,31 +54,103 @@ $(document).ready(function() {
 
                 //var fontSize = ((hInPixels * wInPixels) / 2000) - innerText.length / 2;  //randomWidth;
                 //console.log(fontSize);
-				var alignment;
-				var alignValue = Math.random()
-				if (alignValue < 0.33){
-					alignment = "left";
-				}else if (alignValue > 0.66){
-					alignment = "right";
-				}else{
-					alignment = "center";
-				}
-                var xfollow2 = "<div id=\'box-" + y + "-" + x + "t\' class='innerBoxesRow' style='height: " + hInPixels + "px; flex: 0 0 auto; text-align: " + alignment +"; font-size: 10pt'><span>" + innerText + "</span></div>";
+                var alignment;
+                var alignValue = Math.random()
+                if (alignValue < 0.33)
+                {
+                    alignment = "left";
+                }
+                else if (alignValue > 0.66)
+                {
+                    alignment = "right";
+                }
+                else
+                {
+                    alignment = "center";
+                }
+
+                var xfollow2;
+
+                if (AroundPointRatio(wInPixels, hInPixels, 240, 180, 0.2) && !done[0])
+                {
+                    xfollow2 = "<div id=\'box-" + y + "-" + x + "t\' class='innerBoxesRow' style='height: " + hInPixels + "px; flex: 0 0 auto; text-align: " + alignment + "; font-size: 10pt'><video id='sheppardPortrait' width='" + wInPixels + "' height='" + hInPixels + "' autoplay loop muted><source src='Media/sheppard.mp4' type='video/mp4'></video></div>";
+                    done[0] = true;
+                }
+                else if (AroundPointRatio(wInPixels, hInPixels, 937, 379, 0.2) && !done[1])
+                {
+                    xfollow2 = "<div id=\'box-" + y + "-" + x + "t\' class='innerBoxesRow' style='height: " + hInPixels + "px; flex: 0 0 auto; text-align: " + alignment + "; font-size: 10pt'> <img src='Media/RespectWords.png' alt='Respect' height=" + hInPixels + " width=" + wInPixels + "> </div>";
+                    done[1] = true;
+                }
+                else
+                {
+                    xfollow2 = "<div id=\'box-" + y + "-" + x + "t\' class='innerBoxesRow' style='height: " + hInPixels + "px; flex: 0 0 auto; text-align: " + alignment + "; font-size: 10pt'><span>" + innerText + "</span></div>";
+                }
 
                 $("#vertBox-" + y).append(xfollow);
                 $("#vertBox2-" + y).append(xfollow2);
             }
         }
+        $("#sheppardPortrait").playbackRate = 1.6;
+        $(".innerBoxesRow").click(function()
+        {
+            $(this).addClass("magictime puffOut");
+            var thisIs = this;
+            setTimeout(function()
+            {
+                $(thisIs).addClass('loaded');
+                $(thisIs).css("pointerEvents", "none");
+                //  $(thisIs).css("position", "fixed");
+            }, 1000);
+        });
     }
 
     var resizeTimer;
-    $(window).resize(function() {
+    $(window).resize(function()
+    {
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(Resize, 100);
     });
 });
 
-function RandomSplits(total, amountOfValues, min, max) {
+function AroundPointRatio(width, height, preferedWidth, preferedHeight, allowence)
+{
+    var ratio = width / height;
+    var prefRatio = preferedWidth / preferedHeight;
+
+    if (NearlyEqual(ratio, prefRatio, allowence))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    // if (width > preferedWidth - allow && width < preferedWidth + allow) {
+    //     if (height > preferedHeight - allow && height < preferedHeight + allow) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // } else {
+    //     return false;
+    // }
+}
+
+function NearlyEqual(a, b, allowence)
+{
+    if (a > b - allowence && a < b + allowence)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function RandomSplits(total, amountOfValues, min, max)
+{
     // while (true) {
     //     var a = [];
     //     var i = 0;
@@ -100,25 +172,29 @@ function RandomSplits(total, amountOfValues, min, max) {
     // }
     var trueTotal = 0;
     var a = [];
-    for (var i = 0; i < amountOfValues; i++) {
-      var currentPercent = GetRandomArbitrary(min, max);
-      trueTotal += currentPercent;
-      a.push(currentPercent);
+    for (var i = 0; i < amountOfValues; i++)
+    {
+        var currentPercent = GetRandomArbitrary(min, max);
+        trueTotal += currentPercent;
+        a.push(currentPercent);
     }
-    for (var i = 0; i < a.length; i++) {
-      a[i] = a[i] / trueTotal;
-      a[i] = a[i] * total;
+    for (var i = 0; i < a.length; i++)
+    {
+        a[i] = a[i] / trueTotal;
+        a[i] = a[i] * total;
     }
     //a = shuffle(a);
     return a;
 }
 
-function shuffle(array) {
+function shuffle(array)
+{
     var currentIndex = array.length,
         temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+    while (0 !== currentIndex)
+    {
 
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -133,118 +209,149 @@ function shuffle(array) {
     return array;
 }
 
-function Resize() {
+function Resize()
+{
     $(".secondBody").css("top", $(".mainBody").offset().top);
     $(".secondBody").css("left", $(".mainBody").offset().left);
     $(".secondBody").css("width", $(".mainBody").width());
     $(".secondBody").css("height", $(".mainBody").height());
 
-    $(".secondBody").find("*").textfill({minFontPixels: 5, maxFontPixels:900});
+    $(".secondBody").find("*").textfill(
+    {
+        minFontPixels: 5,
+        maxFontPixels: 900
+    });
 }
 
 var clicked = false;
 
-function GetRandomInt(min, max) {
+function GetRandomInt(min, max)
+{
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function GetRandomArbitrary(min, max) {
+function GetRandomArbitrary(min, max)
+{
     return Math.random() * (max - min) + min;
 }
 
-function SnellClick(e) {
-    if (clicked == false) {
+function SnellClick(e)
+{
+    if (clicked == false)
+    {
         clicked = true;
         $("#snellGlow").css("left", e.pageX);
         $("#snellGlow").css("top", e.pageY);
         $("#snellGlow").css("box-shadow", "0 0 10px 0 rgba(0, 0, 0, 0.9)");
-        $("#snellGlow").animate({
+        $("#snellGlow").animate(
+        {
             boxShadow: "0 0 9000000px 1850px rgba(255, 255, 255, 1)"
-        }, {
+        },
+        {
             duration: 1500,
             easing: "linear"
         });
         $("#snellGlow").css("z-index", "10000");
         //  $(".Snell").css("z-index", "-1");
-        setTimeout(function delayed() {
+        setTimeout(function delayed()
+        {
             GoToPageFromSheppard("Snell");
         }, 2000);
     }
 }
 
-function HillaryClick(e) {
-    if (clicked == false) {
+function HillaryClick(e)
+{
+    if (clicked == false)
+    {
         clicked = true;
         $("#hillaryGlow").css("left", e.pageX);
         $("#hillaryGlow").css("top", e.pageY);
         $("#hillaryGlow").css("box-shadow", "0 0 10px 0 rgba(250, 255, 0, 0.9)");
-        $("#hillaryGlow").animate({
+        $("#hillaryGlow").animate(
+        {
             boxShadow: "0 0 9000000px 1850px rgba(255, 255, 255, 1)"
-        }, {
+        },
+        {
             duration: 1500,
             easing: "linear",
             // done: GoToPage("Hillary")
         });
         $("#hillaryGlow").css("z-index", "10000");
         //  $(".Hillary").css("z-index", "-1");
-        setTimeout(function delayed() {
+        setTimeout(function delayed()
+        {
             GoToPageFromSheppard("Hillary");
         }, 2000);
     }
 }
 
-function RutherfordClick(e) {
-    if (clicked == false) {
+function RutherfordClick(e)
+{
+    if (clicked == false)
+    {
         clicked = true;
         $("#rutherfordGlow").css("left", e.pageX);
         $("#rutherfordGlow").css("top", e.pageY);
         $("#rutherfordGlow").css("box-shadow", "0 0 10px 0 rgba(255, 0, 0, 0.9)");
-        $("#rutherfordGlow").animate({
+        $("#rutherfordGlow").animate(
+        {
             boxShadow: "0 0 8000000px 3000px rgba(255, 255, 255, 1)"
-        }, {
+        },
+        {
             duration: 1500,
             easing: "linear"
         });
         $("#rutherfordGlow").css("z-index", "1000");
-        setTimeout(function delayed() {
+        setTimeout(function delayed()
+        {
             GoToPageFromSheppard("Rutherford");
         }, 2000);
     }
 }
 
-function TepueaClick(e) {
-    if (clicked == false) {
+function TepueaClick(e)
+{
+    if (clicked == false)
+    {
         clicked = true;
         $("#tepueaGlow").css("left", e.pageX);
         $("#tepueaGlow").css("top", e.pageY);
         $("#tepueaGlow").css("box-shadow", "0 0 10px 0 rgba(0, 198, 26, 0.9)");
-        $("#tepueaGlow").animate({
+        $("#tepueaGlow").animate(
+        {
             boxShadow: "0 0 8000000px 3000px rgba(255, 255, 255, 1)"
-        }, {
+        },
+        {
             duration: 1500,
             easing: "linear"
         });
         $("#tepueaGlow").css("z-index", "1000");
-        setTimeout(function delayed() {
+        setTimeout(function delayed()
+        {
             GoToPageFromSheppard("Tepuea");
         }, 2000);
     }
 }
 
-function GoToPageFromSheppard(page) {
+function GoToPageFromSheppard(page)
+{
     window.location.href = "../" + page + "/index.html";
 }
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max)
+{
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function FadeOut() {
+function FadeOut()
+{
     $("#loaderWrapper").addClass("magictime puffOut");
     //ResizeCovers();
-    setTimeout(function() {
+    setTimeout(function()
+    {
         $("#loaderWrapper").addClass('loaded');
     }, 1000);
 }
